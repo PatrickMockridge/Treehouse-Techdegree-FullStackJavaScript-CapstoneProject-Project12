9841bee3b948dcc62f8050d210fa6b3732b80aba
Patrick Mockridge <patrick@engzig.com>
2016-09-05 11:33:45 +0100

    Updated
    Updated Routes

--- app/client/scripts/services/placeService.js
+++ app/client/scripts/services/placeService.js
@@ -9,22 +9,22 @@ angular
   .service('dataServicePlaces', ['$http', function($http) {
       // get all places
      this.getAll = function(callback) {
-       $http.get('http://localhost:5000/api/places')
+       $http.get('/api/places')
             .then(callback);
      };
      // get specific place
     this.getID = function(id, callback) {
-      $http.get('http://localhost:5000/api/places/' + id)
+      $http.get('/api/places/' + id)
            .then(callback);
      };
      // update a place
     this.putID = function(id, data, callback, failure) {
-     $http.put('http://localhost:5000/api/places/' + id, data)
+     $http.put('/api/places/' + id, data)
           .then(callback, failure);
      };
      // add a new place
      this.addPlace = function(place, callback, failure) {
-       $http.post('http://localhost:5000/api/places', place)
+       $http.post('/api/places', place)
             .then(callback, failure);
      };
 }]);
